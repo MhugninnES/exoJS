a8fc7a8e508fb707e978524f0d3a24c4c4d841b5
MhugninnES <emilien.schmitter1@gmail.com>
2025-02-15 20:55:25 +0100

    feat: starting with some basic shifumi script

--- shi_fu_mi/script.js
+++ shi_fu_mi/script.js
@@ -1,4 +1,26 @@
-function randomizer(a, b){
-    let random = Math.random(a);
-    console.log(random);
+function randomizer(){
+    let random = Math.floor(Math.random()* 3)+1;
+    return random;
+}
+
+function ShiFuMi(){
+    let stringResult = " ";
+    let result = randomizer();
+    console.log(result);
+    if(result === 1){
+        stringResult = "Pierre";
+    }else if(result === 2){
+        stringResult = "Papier";
+    }else{
+        stringResult = "Ciseau";
+    }
+    console.log(stringResult);
+}
+
+function startGame(){
+    ShiFuMi();
+}
+
+function resetGame(){
+
 }
